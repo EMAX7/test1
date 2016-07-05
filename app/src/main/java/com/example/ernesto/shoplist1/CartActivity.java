56ad2dbed9cac2b98546258d3e93daf054b1f2e2
EMAX7 <maximino.ernesto@gmail.com>
2016-07-05 17:59:35 -0300

    Segundo

--- app/src/main/java/com/example/ernesto/shoplist1/CartActivity.java
+++ app/src/main/java/com/example/ernesto/shoplist1/CartActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import com.example.ernesto.shoplist1.R;
 
 public class CartActivity extends AppCompatActivity {
+    //sadasdasdasd
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
